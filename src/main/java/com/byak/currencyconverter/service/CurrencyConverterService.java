cc4fb75f99bb2dcdd980cd634c53421ee81a9a6d
Maria Beketova <byaketava@gmail.com>
2024-02-27 02:26:42 +0300

    change constant names

--- src/main/java/com/byak/currencyconverter/service/CurrencyConverterService.java
+++ src/main/java/com/byak/currencyconverter/service/CurrencyConverterService.java
@@ -6,12 +6,12 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CurrencyConverterService {
-    private static final String apiUrl = "https://api.getgeoapi.com/v2/currency/convert";
-    private static final String apiKey = "592c7851435b279c868970e0c7955dd286cdef1f";
+    private static final String API_URL = "https://api.getgeoapi.com/v2/currency/convert";
+    private static final String API_KEY = "592c7851435b279c868970e0c7955dd286cdef1f";
 
     public CurrencyConverterResponse convertCurrency(String fromCurrency, String toCurrency,
                                                      double amount) {
-        String url = apiUrl + "?api_key=" + apiKey + "&from=" + fromCurrency + "&to="
+        String url = API_URL + "?api_key=" + API_KEY + "&from=" + fromCurrency + "&to="
                      + toCurrency + "&amount=" + amount + "&format=json";
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForObject(url, CurrencyConverterResponse.class);

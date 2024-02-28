ec1b8760845a90b48816346e3291df3d3799d2dd
Maria Beketova <byaketava@gmail.com>
2024-02-28 21:52:35 +0300

    secure api key storage

--- src/main/java/com/byak/currencyconverter/controller/CurrencyConverterController.java
+++ src/main/java/com/byak/currencyconverter/controller/CurrencyConverterController.java
@@ -9,14 +9,12 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 @RequestMapping("/convert")
 public class CurrencyConverterController {
-
-    private  CurrencyConverterService currencyConverterService;
+    private CurrencyConverterService currencyConverterService;
 
     @GetMapping("/{fromCurrency}/{toCurrency}/{amount}")
-    public CurrencyConverterResponse convertCurrency(
-            @PathVariable String fromCurrency,
-            @PathVariable String toCurrency,
-            @PathVariable double amount) {
+    public CurrencyConverterResponse convertCurrency(@PathVariable String fromCurrency,
+                                                     @PathVariable String toCurrency,
+                                                     @PathVariable double amount) {
         return currencyConverterService.convertCurrency(fromCurrency, toCurrency, amount);
     }
 }
--- src/main/java/com/byak/currencyconverter/service/CurrencyConverterService.java
+++ src/main/java/com/byak/currencyconverter/service/CurrencyConverterService.java
@@ -1,18 +1,20 @@
 package com.byak.currencyconverter.service;
 
 import com.byak.currencyconverter.model.CurrencyConverterResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CurrencyConverterService {
+    @Value("${api.key}")
+    private String apiKey;
     private static final String API_URL = "https://api.getgeoapi.com/v2/currency/convert";
-    private static final String API_KEY = "592c7851435b279c868970e0c7955dd286cdef1f";
 
     public CurrencyConverterResponse convertCurrency(String fromCurrency, String toCurrency,
                                                      double amount) {
-        String url = API_URL + "?api_key=" + API_KEY + "&from=" + fromCurrency + "&to="
-                     + toCurrency + "&amount=" + amount + "&format=json";
+        String url = API_URL + "?api_key=" + apiKey + "&from=" + fromCurrency
+                + "&to=" + toCurrency + "&amount=" + amount + "&format=json";
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForObject(url, CurrencyConverterResponse.class);
     }
